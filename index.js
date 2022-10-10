52ad050583d0c9ea0671800301fe2766f8561044
Inna <inna.kornieko@gmail.com>
2022-10-10 13:50:11 +0300

    add main file

---
+++ index.js
@@ -0,0 +1,2 @@
+import './js/modal-window.js';
+import './js/form.js';
---
+++ js/form.js
@@ -0,0 +1,90 @@
+
+// const STORAGE_KEY = 'feedback-form-state';
+// const formData = {};
+
+// updateForm();
+
+// 
+
+// function onFormInput(e) {
+//     formData[e.target.name] = e.target.value;
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+//     console.log(formData);
+// };
+
+// function onFormSubmit(e) {
+//     e.preventDefault();
+//     const {
+//         elements: { name, email, tel }
+//       } = e.target;
+          
+//       if (name.value ==="" || email.value === "" || tel.value === "") {
+//         return window.alert("Please fill in all the fields!");
+//       };
+
+//       if (name.value.length < 2 && email.value.length < 2 && tel.value.length < 2 ) {
+//         return window.alert("Value must be more than 2 symbols")
+//       };
+
+//       return window.alert("Form submitted successfully");
+
+//     console.log({'Name': name.value, 'Email': email.value, 'Tel': tel.value});
+//     formEl.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+    
+// };
+
+
+
+
+
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
+
+const email = formEl.email.value;
+console.log(email);
+
+const STORAGE_KEY = 'form-state';
+const formData = {};
+
+formEl.addEventListener('input', onFormInput);
+formEl.addEventListener('submit', onFormSubmit);
+
+updateForm();
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
+};
+
+
+function onFormSubmit(e) {
+      e.preventDefault();
+      const {
+          elements: { name, email, tel }
+        } = e.target;
+
+        if (name.value.length < 2) {
+          return window.alert("Name value must be more than 2 symbols")
+        } else             
+        if (name.value ==="" || email.value === "" || tel.value === "") {
+          return window.alert("Please fill in all the fields!");
+        };
+      console.log({'Name': name.value, 'Email': email.value, 'Tel': tel.value});
+      window.alert("Form submitted successfully");
+      formEl.reset();
+      localStorage.removeItem(STORAGE_KEY);
+      
+  };
+
+function updateForm() {
+    if (localStorage.getItem(STORAGE_KEY) === null) {
+return
+    }
+    const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      Object.entries(savedForm).forEach(([ name, value ]) => {
+        formData[name] = value;
+        formEl.elements[name].value = value;
+      });
+};
